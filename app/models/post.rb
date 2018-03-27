e6e8a4c6c1bdc919741c006da7aaff1a01fa523a
Berry  Semexan <bsemexan@gmail.com>
2018-03-27 00:56:40 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :is_clickbait?
 
-  def is_clickbait
+  def is_clickbait?
     if
       ["Won't Believe", "Secret", "Top", "Guess"].none? { |pat| pat.match title }
       errors.add(:title, "must be clickbait")
